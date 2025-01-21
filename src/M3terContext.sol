23b2dbfb3c484fdc1bcbd89734d69090d2263d99
ichristwin <ichristwin@gmail.com>
2025-01-21 12:51:45 +0100

    made context upgradable

--- src/M3terContext.sol
+++ src/M3terContext.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.24;
 
 import {IM3terContext} from "./interfaces/IM3terContext.sol";
-import {IERC721} from "@openzeppelin/contracts-upgradeable@5.1.0/interfaces/IERC721.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable@5.1.0/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.1.0/proxy/utils/UUPSUpgradeable.sol";
-import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable@5.1.0/access/AccessControlUpgradeable.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable@5.2.0/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable@5.2.0/access/AccessControlUpgradeable.sol";
 
 contract M3terContext is IM3terContext, Initializable, AccessControlUpgradeable, UUPSUpgradeable {
     IERC721 public constant M3TER = IERC721(0x0000000000000000000000000000000000000000); // ToDo: set actual m3ter contract
